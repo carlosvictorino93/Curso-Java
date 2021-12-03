00cbba99ad3ab53f81cd8d9da18a31b41758ae59
carlosvictorino93 <carlos.victorino@alumni.usp.br>
2021-12-03 20:01:38 -0300

    Class 3 Exercise 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,14 +5,20 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        Quadrado quadrado = new Quadrado(188, 244);
-        quadrado.printArea();
-        Triangulo triangulo = new Triangulo(188, 244);
-        triangulo.printArea();
-        Notas notas = new Notas(9.5f,5.7f,8f);
-        notas.printMean();
-        Avatar avatar = new Avatar("X", AvatarElements.FOGO);
-        avatar.printAvatar();
+//        Quadrado quadrado = new Quadrado(188, 244);
+//        quadrado.printArea();
+//        Triangulo triangulo = new Triangulo(188, 244);
+//        triangulo.printArea();
+//        Notas notas = new Notas(9.5f,5.7f,8f);
+//        notas.printMean();
+//        Avatar avatar = new Avatar("X", AvatarElements.FOGO);
+//        avatar.printAvatar();
+
+        String name = "Carlos";
+        int age = 27;
+        float height = 1.7f;
+
+        System.out.printf("My name is %s,%nI am %d and%nI am %.2f tall.", name, age, height);
     }
 
 
